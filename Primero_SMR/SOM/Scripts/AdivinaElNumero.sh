486cc8840e40c0a2b91877c96385233d88005c37
kike1984 <enrique_ma_a@hotmail.com>
2023-09-27 12:02:30 +0200

    Actualizaciones a dia 27 de septiembre.

--- Primero_SMR/SOM/Scripts/AdivinaElNumero.sh
+++ Primero_SMR/SOM/Scripts/AdivinaElNumero.sh
@@ -1,25 +1,25 @@
-#!/bin/zsh
+#!/bin/bash
 
 numeroSecreto=10
 
 echo Escribe un número del 1 al 20
-read numero
+read -r numero
 
 # Validación del imput
 if [[ -z "${numero##*[!0-9]*}" ]]; then
-  echo El valor introducido no es correcto!!!
-  exit
+	echo El valor introducido no es correcto!!!
+	exit
 fi
 
 if [[ $numeroSecreto -eq "$numero" ]]; then
-  echo Bieeeeennnn!!! Has hacertado el número secreto!!!
-  exit
+	echo Bieeeeennnn!!! Has hacertado el número secreto!!!
+	exit
 fi
 
 echo OOOOHHhh!!! No has acertado el número secreto!
 
 if [[ $numeroSecreto -lt "$numero" ]]; then
-  echo El número secreto es menor!
+	echo El número secreto es menor!
 else
-  echo El número secreto es mayor!
+	echo El número secreto es mayor!
 fi
